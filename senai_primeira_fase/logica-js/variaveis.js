919b4a336e3b1488312f3baafa498e3ff7b5f5fd
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-09-09 22:38:53 -0300

    variaveis em js

---
+++ senai_primeira_fase/logica-js/variaveis.js
@@ -0,0 +1,23 @@
+// variavel com var
+// var nome = "gabriel"
+// var nome = "amabili"
+// nome "gabriel"
+
+// variavel com let
+// let nome = "gabriel"
+// nome = "zezinha"
+
+// variavel declarada com const
+const nome = "gabriel" // string
+const sobrenome = "ferraz"
+const idade = 17 // number
+const masculino = true // boolean
+
+// array
+const frutas = ["laranja", "maçã", "banana" ]
+console.log(frutas[0])
+
+// console.log("meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+
