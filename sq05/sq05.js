6c5a46051da5deb155b050fe87cd39011720d575
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-09-22 19:41:07 -0300

    sq05

---
+++ sq05/sq05.js
@@ -0,0 +1,15 @@
+let placa
+placa = Number(prompt("Último número: "))
+if(placa == 0 || placa == 1){
+    alert("Não posso rodar na segunda-feira")
+}else if(placa == 2 || placa == 3){
+    alert("Não posso rodar na terça-feira")
+}else if(placa == 4 || placa == 5){
+    alert("Não posso rodar quarta-feira")
+}else if(placa == 6 || placa == 7){
+    alert("Não posso rodar quinta-feira")
+}else if(placa == 8 || placa == 9){
+    alert("Não posso rodar sexta-feira")
+}else {
+    alert("Sai do celular e escreve direito")
+}
