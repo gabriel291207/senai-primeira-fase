a98a291cd81387e84f67063c4aed9217f7fbcb2e
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-09-22 21:20:50 -0300

    sq05

---
+++ sq05/sq06.js
@@ -0,0 +1,11 @@
+let pontuacao
+pontuacao = Number(prompt("Digite sua pontuação: "))
+
+if(pontuacao <=10){
+    alert("Deus ruim!")
+} else if(pontuacao > 10 && pontuacao <=100){
+    alert("Amanhã é um novo dia!")
+} else if(pontuacao > 100 && pontuacao <= 200){
+    alert("Parabéns")
+}else if(pontuacao > 200){
+     alert("Arrasou👍👍")
