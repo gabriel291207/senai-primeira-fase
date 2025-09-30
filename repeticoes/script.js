f70caff78014c202ca56392bb8f0c81940bbd0b0
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-09-29 22:16:10 -0300

    repetições

---
+++ repeticoes/script.js
@@ -0,0 +1,13 @@
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert("Sua nota: " + nota)
+
+
+let contador = 0
+
+while(contador < 2){
+    console.log("Contador: " + contador)
+    contador = contador + 1
+}
