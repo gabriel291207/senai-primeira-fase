48e6a5db9beba07d20d1247289bc7df73b0c8379
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-09-30 22:01:28 -0300

    funcoes

---
+++ logica-js/for.js
@@ -0,0 +1,34 @@
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+
+// for(let i = 0; i <=10; i += 2){
+//     console.log(i)
+// }
+
+
+// let soma = 0
+// for(let i = 1; i < 100; i++)[
+//     soma += i
+// ]
+// console.log("Resultado da soma ", soma)
+
+
+// let alunos = ["Gabriel", "Amabili", "Jose"]
+// alunos.push("zezinha")
+// for(let i = 0;  i < alunos.length; i++){
+//     console.log("aluno: ", alunos[i])
+// }
+// // console.log(alunos.length)
+
+
+let frutas = ["abacaxi", "morango", "maça", "banana", "melancia"]
+for(let i = 0; i < frutas.length; i++){
+    console.log("frutas: ", frutas[i])
+}
---
+++ logica-js/funcoes.js
@@ -0,0 +1,21 @@
+// function saudacao(nome){
+//     console.log(`bom dia ${nome}`)
+// }
+
+// saudacao("gabriel")
+// saudacao("amabili")
+
+
+// const saudacao = (nome) =>{
+//     console.log(`bom dia, ${nome}`)
+// }
+
+// saudacao("gabriel")
+// saudacao("amabili")
+
+
+const soma = (x,y) => x+y
+
+const soma = (x,y) =>{
+    return x=y
+}
