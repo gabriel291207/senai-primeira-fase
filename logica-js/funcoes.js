a8842b8341e3711f951349462cc813fd1a10ffb8
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-09-30 22:10:01 -0300

    funcoes

--- logica-js/funcoes.js
+++ logica-js/funcoes.js
@@ -15,7 +15,15 @@
 
 
 const soma = (x,y) => x+y
+console.log(soma(2,10))
 
-const soma = (x,y) =>{
-    return x=y
-}
+// const soma = (x,y) =>{
+//     return x=y
+// }
+
+const subtracao = (x,y) => x-y
+console.log(subtracao(18,6))
+
+
+const multiplicacao = (x,y) => x*y
+console.log(multiplicacao(6,2))
