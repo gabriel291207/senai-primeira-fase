df373143540b7a61c43f0e0d2c48fddaa0bc09ac
GABRIEL CORDEIRO FERRAZ <gabriel_ferraz150@estudante.sesisenai.org.br>
2025-10-02 22:17:46 -0300

    for

---
+++ repeticoes/for.js
@@ -0,0 +1,42 @@
+//  let i = 0
+//  while (i < 3){
+//     alert("Valor de i: " + i)
+//  }
+
+
+//  for(let i = 0; i < 3; i++)^{
+//     alert("Valor de i: " + i)
+//  }
+
+// let soma = 0
+//  for(let i = 0; i < 10; i++){
+//     let nota = Number(prompt(`Digite ${i+1} a nota`))
+//     soma += nota 
+//  }
+//  alert("A soma das notas é: " + soma)
+
+
+//  const nomes = ["gabriel", "amabili", "joão", "iago"]
+//  for(let i = 0; i < 4; i++){
+//     alert (nomes[i])
+//  }
+
+
+
+//ex2
+// for(let i = 10; i > 0; i--){
+//     alert("Valor de i: " + i)
+// }
+
+// ex4
+// let tabuada = 0
+// for(let i = 0; i <= 10; i++){
+//     tabuada = 5 * i
+//     alert(`5 x ${i}= ${tabuada}`)
+// }
+
+let soma = 0
+for(let i = 1; i < 100; i++)[
+    soma += i
+]
+ console.log("Resultado da soma ", soma)
